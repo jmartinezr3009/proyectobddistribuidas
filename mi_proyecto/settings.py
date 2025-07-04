f39f391c72305e0330d85a4ed5e0d7c513e99956
jmartinezr3009 <julianamartinezroa@gmail.com>
2025-07-03 21:03:21 -0500

    se corrigio pdf

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -110,11 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
+TIME_ZONE = 'America/Bogota'
 
 USE_TZ = True
 
